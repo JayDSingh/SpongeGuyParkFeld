bf8b30b48bbdb1d3599ed7b21a542d05b32f1f5c
Mikaela-G <32914691+Mikaela-G@users.noreply.github.com>
2018-02-12 20:23:20 -0800

    Add files via upload

---
+++ teamliam/urlscraping.py
@@ -0,0 +1,16 @@
+#compiles all the transcript links into a list
+
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+transcripts = "http://spongebob.wikia.com/wiki/List_of_transcripts#Season"
+
+rawHTML = urlopen(transcripts).read()
+
+soup = BeautifulSoup(rawHTML, "html.parser")
+
+links = []
+for link in soup.find_all("table")[1].find_all("a"):
+    links += ["http://spongebob.wikia.com/wiki/List_of_transcripts#Season" + link.get("href").strip()]
+newlinks = [ link for link in links if u"(transcript)" in link ]
+print(newlinks)
