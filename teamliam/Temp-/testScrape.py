e1b1c5003368b361f63caa26d6f2ba951e70688b
jerryliu12 <jerryliu@umail.ucsb.edu>
2018-03-02 19:37:14 -0800

    CSV file in the wrong format

---
+++ teamliam/Temp-/testScrape.py
@@ -0,0 +1,63 @@
+#Author: Jerry Liu
+#Today is February 23, 2018.
+#This is the first test scraper for the Spongebob scripts. 
+#What I have done: I have learned and accomplished how to get the script from 
+#the 1st episode by following the plan I previously laid out in the test 
+#folder. I have gotten all the lines with the tags into the variable called
+#lines. We can take the text out using .get_text(), but we want to retain
+#all the css tags so that it is easier to put the data into a csv file!
+#Working on how to put the lines into the csv file is the next step.
+
+
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import csv
+import re
+
+#type(html_soup)
+#bs4.BeautifulSoup
+
+firstEpi = "http://spongebob.wikia.com/wiki/Help_Wanted_(transcript)"
+firstHTML = urlopen(firstEpi).read()
+
+html_soup = BeautifulSoup(firstHTML,'html.parser')
+
+#find the <ul> content, aka the actual script
+content = html_soup.find('ul', attrs={'class': None})
+
+#find each bullet
+lines = content.findAll('li')
+
+#for x in lines:
+	#print(x.getText())
+
+#print(lines)
+
+lineList = []
+
+for x in lines:
+	lineList.append(re.sub("[\(\[].*?[\)\]]", "", x.getText()))
+
+#csv.register_dialect('colon', delimiter=':')
+
+# take the character names
+def get_character(line):
+    x = re.search("^[^:]*:*", line) # and not followed by another :?
+    y = x.group(0)
+    y = re.sub(":", "", y)
+    return y
+
+# take the dialogue
+def get_dialogue(line):
+    x = re.search(":+.*", line)
+    y = x.group(0)
+    y = re.sub("^[^:]*:\s*", "", y)
+    return y
+
+with open('testline.csv', 'w', newline='') as csvfile:
+    testwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for x in lineList:
+    	testwriter.writerow(get_characterI)
+
+#print(html_soup.prettify())
