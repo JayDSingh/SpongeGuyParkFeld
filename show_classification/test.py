a45917af3bd8f78de2dab39f1f5b2c7619c039b2
JayDSingh <jay@umail.ucsb.edu>
2018-04-22 17:21:31 -0700

    Add files via upload
    basic tokenizing in nltk

--- show_classification/test.py
+++ show_classification/test.py
@@ -1,1 +1,7 @@
-
+import csv
+import nltk
+input_file = open("All-seasons.csv", "r")
+for row in csv.reader(input_file):
+    sentence = str(row)
+    tokens = nltk.word_tokenize(sentence)
+    print (tokens)
