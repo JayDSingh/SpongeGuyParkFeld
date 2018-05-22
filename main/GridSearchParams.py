f1a0c3941f3eda8659ad94f7565ff66380b7097a
liamtabrams <34357178+liamtabrams@users.noreply.github.com>
2018-05-22 15:22:25 -0700

    Update GridSearchParams.py

--- main/GridSearchParams.py
+++ main/GridSearchParams.py
@@ -9,11 +9,11 @@ from sklearn.model_selection import GridSearchCV
 text_clf = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', MultinomialNB())])
 parameters = {'vect__ngram_range': [(1, 1), (1, 2)],'tfidf__use_idf': (True, False),'clf__alpha': (1e-2, 1e-3)}
 count_vect = CountVectorizer()
-input_file = open("All-seasons.csv", "r", encoding="utf8")
-L = []
-T = []
-J = []
-acc = 0
+input_file = open("All-seasons.csv", "r", encoding="utf8") #open the South Park csv
+L = [] #list of lines for training data
+T = []  #list of lines for test data
+J = []  #list of tags for test data
+acc = 0 #counter variable
 for row in csv.reader(input_file):
     if acc < 60000:
         L.append(str(row[3]))
@@ -25,18 +25,18 @@ for row in csv.reader(input_file):
 
 
 
-input_file1 = open("bigcsv1.csv", "r", encoding="utf8")
-bacc = 0
+input_file1 = open("bigcsv1.csv", "r", encoding="utf8") #open the SpongeBob csv
+acc1 = 0 #new counter variable
 for row in csv.reader(input_file1):
-    if bacc < 40000:
+    if acc1 < 40000:
         L.append(str(row[1]))
-        bacc += 1
-    if bacc == 40000:
+        acc1 += 1
+    if acc1 == 40000:
         T.append(str(row[1]))
         J.append('SS')
 
 
-P = []
+P = [] #training data tags
 X_train_counts = count_vect.fit_transform(L)
 for i in range(0,60000):
     P.append('SP')
@@ -46,8 +46,8 @@ tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 clf = MultinomialNB().fit(X_train_tfidf, P)
 text_clf = text_clf.fit(L, P)
-predicted = []
-predicted = text_clf.predict(T)
+predicted = [] #empty list
+predicted = text_clf.predict(T) #list of predicted values for the test tags
 gs_clf = GridSearchCV(text_clf, parameters, n_jobs=-1)
 gs_clf = gs_clf.fit(L, P)
 print(gs_clf.best_score_)
