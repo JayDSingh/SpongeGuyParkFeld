4ee35a76dcd9700748fa54492eddfbdb04694493
Evan Azevedo <evanazzvd@gmail.com>
2018-05-27 17:06:44 -0700

    Update word2vec.py

--- temp/word2vec.py
+++ temp/word2vec.py
@@ -1,14 +1,59 @@
-import tensorflow as tf
-import re
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Basic word2vec example."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import math
 import os
+import sys
+import argparse
+import random
+from tempfile import gettempdir
+import string
+
 import numpy as np
 import pandas as pd
-import string
-import matplotlib.pyplot as plt
+from six.moves import urllib
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+
+from tensorflow.contrib.tensorboard.plugins import projector
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# Open the transcripts
+# Give a folder path as an argument with '--log_dir' to save
+# TensorBoard summaries. Default is a log folder in current directory.
+current_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--log_dir',
+    type=str,
+    default=os.path.join(current_path, 'log'),
+    help='The log directory for TensorBoard summaries.')
+FLAGS, unparsed = parser.parse_known_args()
+
+# Create the directory for TensorBoard variables if there is not.
+if not os.path.exists(FLAGS.log_dir):
+  os.makedirs(FLAGS.log_dir)
+
+# Step 1: Open the transcripts
 ## Transcripts of interest are Southpark & SpongeBob
 transcript_1 = "southpark_transcript.csv"
 transcript_2 = "spongebob_transcript.csv"
@@ -26,103 +71,278 @@ SP_df['Show'] = 'South Park'
 SB_df = pd.read_csv(path_2, names=['Character', 'Line'])
 SB_df['Show'] = 'SpongeBob'
 
-SB_df['Line'] = 'trats ' + SB_df['Line'] + ' dne' # start ("trats") and end ("dne") tokens
-data = SB_df['Line'].str.cat(sep=' ').lower() # lowercase all strings
-data = ' '.join(word.strip(string.punctuation) for word in data.split()) # remove punctuation
+corpus = SP_df['Line']
+# Remove NA values
+keep = pd.notna(corpus)
+data = corpus[keep]
+# Convert the lines to lowercase words w/o punctuation
+translator = str.maketrans('', '', string.punctuation)
+l = [line.lower().translate(translator).split(' ') for line in data]
+vocabulary = [item for sublist in l for item in sublist]
 
-# The next cell will count the number of different words and assign an id to each
-from collections import Counter
-data_length = 1000
-counts = Counter(data[0:data_length].split(' ')) # using only the first 1000 characters in the string of lyrics
-count_pairs = sorted(counts.items(), key=lambda x: (-x[1], x[0])) # sort first by incidence, then by alpha numeric key
-words, word_count = list(zip(*count_pairs))
-word_to_id = dict(zip(words, range(len(words)))) # get an ID for each word
-id_to_word = dict(zip(range(len(words)), words)) # get a word for each id
+print('Data size', len(vocabulary))
 
-# The next cell will split the data by word and will generate an equivalent array with IDs instead of words.
-all_data_word = data[0:data_length].split(' ') # using only the first 1000 characters in the string of lyrics
-all_data_id = [word_to_id[word] for word in all_data_word]
+# Step 2: Build the dictionary and replace rare words with UNK token.
+vocabulary_size = 50000
 
-C = 2
-N = len(all_data_id)
-train_data = np.zeros((C+1,N-2))
-j = 0
-for i in range(1, N-1):
-    train_data[:,j] = all_data_id[int(i-C/2):int(i+C/2+1)]
-    j += 1
-
-# Now we turn this dense matrix into the sparse matrices that the NN will expect:
-V = len(word_to_id)
-x_tr = np.zeros((N-2,V))
-x_tr[range(N-2),train_data[1,:].astype(int)] = 1
-
-y_tr = np.zeros((N-2,V)).astype(int)
-y_tr[range(N-2),train_data[0,:].astype(int)] = 1
-y_tr[range(N-2),train_data[2,:].astype(int)] = 1
-
-# Now we train the network
-V = len(word_to_id)
-H = 2
-lr = 0.00005
-
-# Try negative sub-sampling
-
-# let's initialize the weights
-W_h = tf.Variable(tf.random_normal((V,H), stddev = 0.01)) # mean=0.0
-b_h = tf.Variable(tf.zeros((1,H)))
-W_o = tf.Variable(tf.random_normal((H,V), stddev = 0.01)) # mean=0.0
-b_o = tf.Variable(tf.zeros((1,V)))
-
-# We now initialize the placeholders for our data. At this point we won't be passing any data in yet
-# Tensorflow build a graph with the information that we are giving it, so it just allocates graph space
-# for the data that we will pass in
-# we will have to tell it the datatype ("float"), and the shape of the data
-X = tf.placeholder("float", shape=[None,V])
-y = tf.placeholder("int32", shape=[None,V])
-
-# we now do the forward pass until we obtain the scores
-h = tf.matmul(X,W_h) + b_h
-scores = tf.matmul(h, W_o) + b_o
-
-# now that we have the scores, we can turn that into probabilities
-y_hat = tf.nn.softmax(scores)
-
-# now we have to do the backpropagation, which in tensorflow will be extremely easy.
-# 1st we compute the cross entropy loss
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=scores))
-
-# 2nd we ask tensorflow to compute the gradients with respect to all the tensorflow "variables"
-# which for us are the trainable weights. We will have to tell it what the objective is, and it will
-# also need a "rate" (lr) with which to proceed toward that objetive using Gradient Descent.
-GD_step = tf.train.AdamOptimizer(lr).minimize(loss)
-
-# everything we have done so far has set up the tensorflow graph, but will not make the neural
-# network learn. For learning to take place, we need to initialize a tensorflow session and initialize
-# all the variables
-sess = tf.Session()
-init = tf.global_variables_initializer()
-sess.run(init)
-
-# you can loop over this to train over more than one epoch.
-nepochs = 20000
-for i in range(nepochs):
-    sess.run(GD_step, feed_dict={X: x_tr, y: y_tr})
-
-# If you want to obtain the accuracy of the network on the training set:
-final_loss = sess.run(loss, feed_dict={X: x_tr, y: y_tr})
-print("The final training loss is: ", final_loss)
-
-# save W_h to visualize the embeddings
-W_h = W_h.eval(session=sess)
+def build_dataset(words, n_words):
+  """Process raw inputs into a dataset."""
+  # Get the most frequent words
+  count = [['UNK', -1]]
+  count.extend(collections.Counter(words).most_common(n_words - 1))
+  # Assign a key to each unique word
+  dictionary = dict()
+  for word, _ in count:
+    dictionary[word] = len(dictionary)
+  data = list()
+  unk_count = 0
+  for word in words:
+    index = dictionary.get(word, 0)
+    if index == 0:  # dictionary['UNK']
+      unk_count += 1
+    data.append(index)
+  count[0][1] = unk_count
+  reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+  return data, count, dictionary, reversed_dictionary
 
 
-sess.close()
+# Filling 4 global variables:
+# data - list of codes (integers from 0 to vocabulary_size-1).
+#   This is the original text but words are replaced by their codes
+# count - map of words(strings) to count of occurrences
+# dictionary - map of words(strings) to their codes(integers)
+# reverse_dictionary - maps codes(integers) to words(strings)
+data, count, dictionary, reverse_dictionary = build_dataset(
+    vocabulary, vocabulary_size)
+del vocabulary  # Hint to reduce memory.
 
-# The initial traning loss is around 9.17 (use as reference to assess training progress)
-# how low could the loss go?
+data_index = 0
 
-for i in range(W_h.shape[0]):
-    plt.scatter(W_h[i,0],W_h[i,1])
-    plt.annotate(id_to_word[i],[W_h[i,0],W_h[i,1]])
 
-plt.show()
+# Step 3: Function to generate a training batch for the skip-gram model.
+def generate_batch(batch_size, num_skips, skip_window):
+  global data_index
+  assert batch_size % num_skips == 0
+  assert num_skips <= 2 * skip_window
+  batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+  labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+  span = 2 * skip_window + 1  # [ skip_window target skip_window ]
+  buffer = collections.deque(maxlen=span)  # pylint: disable=redefined-builtin
+  if data_index + span > len(data):
+    data_index = 0
+  buffer.extend(data[data_index:data_index + span])
+  data_index += span
+  for i in range(batch_size // num_skips):
+    context_words = [w for w in range(span) if w != skip_window]
+    words_to_use = random.sample(context_words, num_skips)
+    for j, context_word in enumerate(words_to_use):
+      batch[i * num_skips + j] = buffer[skip_window]
+      labels[i * num_skips + j, 0] = buffer[context_word]
+    if data_index == len(data):
+      buffer.extend(data[0:span])
+      data_index = span
+    else:
+      buffer.append(data[data_index])
+      data_index += 1
+  # Backtrack a little bit to avoid skipping words in the end of a batch
+  data_index = (data_index + len(data) - span) % len(data)
+  return batch, labels
+
+
+batch, labels = generate_batch(batch_size=8, num_skips=2, skip_window=1)
+
+# Step 4: Build and train a skip-gram model.
+
+batch_size = 128
+embedding_size = 128  # Dimension of the embedding vector.
+skip_window = 1  # How many words to consider left and right.
+num_skips = 2  # How many times to reuse an input to generate a label.
+num_sampled = 64  # Number of negative examples to sample.
+lr = 0.5 # Learning rate
+
+# We pick a random validation set to sample nearest neighbors. Here we limit the
+# validation samples to the words that have a low numeric ID, which by
+# construction are also the most frequent. These 3 variables are used only for
+# displaying model accuracy, they don't affect calculation.
+valid_size = 16  # Random set of words to evaluate similarity on.
+valid_window = 100  # Only pick dev samples in the head of the distribution.
+valid_examples = np.random.choice(valid_window, valid_size, replace=False)
+
+graph = tf.Graph()
+
+with graph.as_default():
+
+  # Input data.
+  with tf.name_scope('inputs'):
+    train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
+    train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+    valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+
+  # Ops and variables pinned to the CPU because of missing GPU implementation
+  with tf.device('/cpu:0'):
+    # Look up embeddings for inputs.
+    with tf.name_scope('embeddings'):
+      embeddings = tf.Variable(
+          tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+      embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+
+    # Construct the variables for the NCE loss
+    with tf.name_scope('weights'):
+      nce_weights = tf.Variable(
+          tf.truncated_normal(
+              [vocabulary_size, embedding_size],
+              stddev=1.0 / math.sqrt(embedding_size)))
+    with tf.name_scope('biases'):
+      nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+
+  # Compute the average NCE loss for the batch.
+  # tf.nce_loss automatically draws a new sample of the negative labels each
+  # time we evaluate the loss.
+  # Explanation of the meaning of NCE loss:
+  #   http://mccormickml.com/2016/04/19/word2vec-tutorial-the-skip-gram-model/
+  with tf.name_scope('loss'):
+    loss = tf.reduce_mean(
+        tf.nn.nce_loss(
+            weights=nce_weights,
+            biases=nce_biases,
+            labels=train_labels,
+            inputs=embed,
+            num_sampled=num_sampled,
+            num_classes=vocabulary_size))
+
+  # Add the loss value as a scalar to summary.
+  tf.summary.scalar('loss', loss)
+
+  # Construct the SGD optimizer using a learning rate of 1.0.
+  with tf.name_scope('optimizer'):
+    optimizer = tf.train.GradientDescentOptimizer(lr).minimize(loss)
+
+  # Compute the cosine similarity between minibatch examples and all embeddings.
+  norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keepdims=True))
+  normalized_embeddings = embeddings / norm
+  valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings,
+                                            valid_dataset)
+  similarity = tf.matmul(
+      valid_embeddings, normalized_embeddings, transpose_b=True)
+
+  # Merge all summaries.
+  merged = tf.summary.merge_all()
+
+  # Add variable initializer.
+  init = tf.global_variables_initializer()
+
+  # Create a saver.
+  saver = tf.train.Saver()
+
+# Step 5: Begin training.
+num_steps = 20001
+
+with tf.Session(graph=graph) as session:
+  # Open a writer to write summaries.
+  writer = tf.summary.FileWriter(FLAGS.log_dir, session.graph)
+
+  # We must initialize all variables before we use them.
+  init.run()
+  print('Initialized')
+
+  average_loss = 0
+  for step in xrange(num_steps):
+    batch_inputs, batch_labels = generate_batch(batch_size, num_skips,
+                                                skip_window)
+    feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+
+    # Define metadata variable.
+    run_metadata = tf.RunMetadata()
+
+    # We perform one update step by evaluating the optimizer op (including it
+    # in the list of returned values for session.run()
+    # Also, evaluate the merged op to get all summaries from the returned "summary" variable.
+    # Feed metadata variable to session for visualizing the graph in TensorBoard.
+    _, summary, loss_val = session.run(
+        [optimizer, merged, loss],
+        feed_dict=feed_dict,
+        run_metadata=run_metadata)
+    average_loss += loss_val
+
+    # Add returned summaries to writer in each step.
+    writer.add_summary(summary, step)
+    # Add metadata to visualize the graph for the last run.
+    if step == (num_steps - 1):
+      writer.add_run_metadata(run_metadata, 'step%d' % step)
+
+    if step % 2000 == 0:
+      if step > 0:
+        average_loss /= 2000
+      # The average loss is an estimate of the loss over the last 2000 batches.
+      print('Average loss at step ', step, ': ', average_loss)
+      average_loss = 0
+
+    # Note that this is expensive (~20% slowdown if computed every 500 steps)
+    if step == num_steps - 1:
+      sim = similarity.eval()
+      for i in xrange(valid_size):
+        valid_word = reverse_dictionary[valid_examples[i]]
+        top_k = 8  # number of nearest neighbors
+        nearest = (-sim[i, :]).argsort()[1:top_k + 1]
+        log_str = 'Nearest to %s:' % valid_word
+        for k in xrange(top_k):
+          close_word = reverse_dictionary[nearest[k]]
+          log_str = '%s %s,' % (log_str, close_word)
+        print(log_str)
+  final_embeddings = normalized_embeddings.eval()
+
+  # Write corresponding labels for the embeddings.
+  with open(FLAGS.log_dir + '/metadata.tsv', 'w') as f:
+    for i in xrange(vocabulary_size):
+      f.write(reverse_dictionary[i] + '\n')
+
+  # Save the model for checkpoints.
+  saver.save(session, os.path.join(FLAGS.log_dir, 'model.ckpt'))
+
+  # Create a configuration for visualizing embeddings with the labels in TensorBoard.
+  config = projector.ProjectorConfig()
+  embedding_conf = config.embeddings.add()
+  embedding_conf.tensor_name = embeddings.name
+  embedding_conf.metadata_path = os.path.join(FLAGS.log_dir, 'metadata.tsv')
+  projector.visualize_embeddings(writer, config)
+
+writer.close()
+
+# Step 6: Visualize the embeddings.
+
+
+# pylint: disable=missing-docstring
+# Function to draw visualization of distance between embeddings.
+def plot_with_labels(low_dim_embs, labels, filename):
+  assert low_dim_embs.shape[0] >= len(labels), 'More labels than embeddings'
+  plt.figure(figsize=(8, 7))  # in inches
+  for i, label in enumerate(labels):
+    x, y = low_dim_embs[i, :]
+    plt.scatter(x, y)
+    plt.annotate(
+        label,
+        xy=(x, y),
+        xytext=(5, 2),
+        textcoords='offset points',
+        ha='right',
+        va='bottom')
+
+  plt.savefig(filename)
+  plt.show()
+
+
+try:
+  # pylint: disable=g-import-not-at-top
+  from sklearn.manifold import TSNE
+  import matplotlib.pyplot as plt
+
+  tsne = TSNE(
+      perplexity=30, n_components=2, init='pca', n_iter=5000, method='exact')
+  plot_only = 500
+  low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
+  labels = [reverse_dictionary[i] for i in xrange(plot_only)]
+  plot_with_labels(low_dim_embs, labels, os.path.join(gettempdir(), 'tsne.png'))
+
+except ImportError as ex:
+  print('Please install sklearn, matplotlib, and scipy to show embeddings.')
+  print(ex)
