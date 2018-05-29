0c456b5cd8117f315be47d2088db3bc3d75e5f59
Lauren Shin <laurenshin15@gmail.com>
2018-05-28 21:13:12 -0700

    no message

--- main/csv processing.R
+++ main/csv processing.R
@@ -1,32 +1,30 @@
-
-
 # finding the six most frequent speakers and their lines from each csv
+# hard-coded and messy, function could be better for this purpose... 
 
 # spongebob and south park and seinfeld
 
-setwd("C:/Users/Lauren Shin/Desktop")
-
+# read in data for each
 sponge = data.frame(read.csv("C:/Users/Lauren Shin/Documents/github/SpongeGuyParkFeld/temp/spongebob_transcript.csv"))
 south = data.frame(read.csv("C:/Users/Lauren Shin/Documents/github/SpongeGuyParkFeld/temp/southpark_transcript.csv"))
 seinfeld = data.frame(read.csv("C:/Users/Lauren Shin/Documents/github/SpongeGuyParkFeld/main/seinfeld_transcript.csv"))
 
-attach(sponge)
-attach(south)
-attach(seinfeld)
-
+# find column names for each, figure out which contains speaker name
 colnames(sponge)
 colnames(south)
 colnames(seinfeld)
+
+# use speaker name to find the 6 most frequent speakers
 freqsponge = names(tail(sort(table(sponge["X"])), 6))
 freqsouth = names(tail(sort(table(south["Character"])), 6))
 freqseinfeld = names(tail(sort(table(seinfeld["Character"])), 6))
 
+# load dplyr for filtering by speakers
 library(dplyr)
 
-spongest = dplyr::filter(sponge, X == freqsponge)
+# finding entries in the spongebob (and others) data that are by the top 6 speakers of spongebob
+spongest = dplyr::filter(sponge, X %in% freqsponge)
+southest = dplyr::filter(south, Character %in% freqsouth)
+seinfeldest = dplyr::filter(seinfeld, Character %in% freqseinfeld)
 
-southest = dplyr::filter(south, Character == freqsouth)
-
-seinfeldest = dplyr::filter(seinfeld, Character == freqseinfeld)
-
+# writes to csv, change names and variables for each
 write.csv(seinfeldest, "six_freq_seinfeld.csv", row.names = FALSE)
