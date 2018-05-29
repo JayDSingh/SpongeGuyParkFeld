b32d8c12b4978030f32928999d017d53a9386002
liamtabrams <34357178+liamtabrams@users.noreply.github.com>
2018-05-28 18:01:39 -0700

    Update and rename StemmingPredict to StemmingPredict.py

--- show_classification/StemmingPredict.py
+++ show_classification/StemmingPredict.py
@@ -1,3 +1,4 @@
+#import the libraries we need
 import csv
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
@@ -5,42 +6,40 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer
 import numpy as np
+from nltk.stem.snowball import SnowballStemmer
+#pipeline our functions to make them easier to call
 text_clf = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', MultinomialNB())])
 count_vect = CountVectorizer()
-input_file = open("All-seasons.csv", "r", encoding="utf8")
-L = []
-T = []
-J = []
-acc = 0
+input_file = open("southpark_transcript.csv", "r", encoding="utf8")
+tr_lines = []
+tr_targets = []
+tst_lines = []
+tst_targets = []
+acc = 0 #accumulator
 for row in csv.reader(input_file):
     if acc < 60000:
-        L.append(str(row[3]))
+        tr_lines.append(str(row[3]))
         acc += 1
     if acc == 60000:
-        T.append(str(row[3]))
-        J.append('SP')
-
-
-
+        tst_lines.append(str(row[3]))
+        tst_targets.append('SP')
 
 input_file1 = open("bigcsv1.csv", "r", encoding="utf8")
-bacc = 0
+bacc = 0 #new accumulator
 for row in csv.reader(input_file1):
     if bacc < 40000:
-        L.append(str(row[1]))
+        tr_lines.append(str(row[1]))
         bacc += 1
     if bacc == 40000:
-        T.append(str(row[1]))
-        J.append('SS')
+        tst_lines.append(str(row[1]))
+        tst_targets.append('SS')
 
-
-P = []
-X_train_counts = count_vect.fit_transform(L)
 for i in range(0,60000):
-    P.append('SP')
+    tr_targets.append('SP')
 for i in range(0,40000):
-    P.append('SS')
-from nltk.stem.snowball import SnowballStemmer
+    tr_targets.append('SS')
+
+X_train_counts = count_vect.fit_transform(tr_lines)
 stemmer = SnowballStemmer("english", ignore_stopwords=True)
 class StemmedCountVectorizer(CountVectorizer):
     def build_analyzer(self):
@@ -48,6 +47,6 @@ class StemmedCountVectorizer(CountVectorizer):
         return lambda doc: ([stemmer.stem(w) for w in analyzer(doc)])
 stemmed_count_vect = StemmedCountVectorizer(stop_words='english')
 text_mnb_stemmed = Pipeline([('vect', stemmed_count_vect),('tfidf', TfidfTransformer()),('mnb', MultinomialNB(fit_prior=False))])
-text_mnb_stemmed = text_mnb_stemmed.fit(L, P)
-predicted_mnb_stemmed = text_mnb_stemmed.predict(T)
-print(np.mean(predicted_mnb_stemmed == J))
+text_mnb_stemmed = text_mnb_stemmed.fit(tr_lines, tr_targets)
+predicted_mnb_stemmed = text_mnb_stemmed.predict(tst_lines)
+print(np.mean(predicted_mnb_stemmed == tst_targets))
